ea061dfced44d04c0cb515fc322bfd056c81e2e4
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-03-12 18:27:39 +0530

    Added loading component

--- src/components/twitterCard.js
+++ src/components/twitterCard.js
@@ -1,11 +1,23 @@
+import React from "react";
 import { TwitterTweetEmbed } from "react-twitter-embed";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const TwitterCard = ({ tweetId }) => {
+	const [loading, setLoading] = React.useState(true);
+
+	const afterComponentLoad = () => {
+		setLoading(false);
+	};
+
 	return (
-		<TwitterTweetEmbed
-			options={{ theme: "dark", width: "auto" }}
-			tweetId={tweetId}
-		/>
+		<div>
+			{loading && <CircularProgress sx={{ color: "#232222" }} />}
+			<TwitterTweetEmbed
+				options={{ theme: "dark", width: "auto" }}
+				onLoad={(element) => afterComponentLoad(element)}
+				tweetId={tweetId}
+			/>
+		</div>
 	);
 };
 
